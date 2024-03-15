2c375510c7c7b2d2bf9a7849178a112920770578
LucasManoel1008 <lucasgabrielsilvamanoel@gmail.com>
2024-03-15 07:44:03 -0300

    Adicionei "Nome"

--- projeto-inicial/scripts.js
+++ projeto-inicial/scripts.js
@@ -1,8 +1,8 @@
 var botao = document.getElementById("nome");
 botao.addEventListener("click", alertar, false);
- 
+ var nome = document.getElementById("nome")
 function alertar(event){
     if(true){
-        alert("Você clicou no botão!!!" + event);
+        alert("Você clicou no botão!!!" + nome.value);
     }
 }
