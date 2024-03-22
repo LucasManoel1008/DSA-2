52f4120fbf96193a5f46df8588db4b9b703ba6d4
LucasManoel1008 <lucasgabrielsilvamanoel@gmail.com>
2024-03-22 08:30:48 -0300

    Cadastro completo

--- projeto-inicial/scripts.js
+++ projeto-inicial/scripts.js
@@ -1,8 +1,37 @@
 var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar, false);
- var nome = document.getElementById("nome")
+ var email = document.getElementById("nome");
+var nome = document.getElementById("saida-de-dados");
+var tel = document.getElementById("tel");
+var cep = document.getElementById("cep");
+var log = document.getElementById("log");
+var num = document.getElementById("num");
+var comp = document.getElementById("comp");
+var bair = document.getElementById("bair");
+var city = document.getElementById("city");
+var esta = document.getElementById("esta");
+
 function alertar(event){
     if(true){
-        alert("Você clicou no botão!!!" + nome.value);
+        //alert("Você clicou no botão!!!" + nome.value);
+
+        const url = `https://viacep.com.br/ws/${cep.value}/json/`;
+
+
+        fetch(url)
+        .then(resposta=>resposta.json())
+        .then(dados=>alert(dados.logradouro))
+
+    nome.innerText = "Nome: "
+    + email.value+ "\n Email:  "
+    +email.value + "\n Telefone:   " 
+    + tel.value + "\n CEP:    " 
+    + cep.value +  "\n Logradouro: " 
+    + log.value + "\n Número: "
+    + num.value + "\n Complemento: " 
+    + comp.value + "\n Bairro:    " 
+    + bair.value + "\n Cidade:    " 
+    + city.value + "\n Estado:    " 
+    + esta.value;
     }
 }
